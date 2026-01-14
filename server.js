85279cddf663e80f6173ebcb884038958e17d96e
GantleM <63802344+GantleM@users.noreply.github.com>
2026-01-14 11:38:29 +0000

    Test code for server

---
+++ server.js
@@ -0,0 +1,28 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({
+  port: process.env.PORT || 8080,
+  host: '0.0.0.0'
+});
+
+server.on('connection', socket => {
+  console.log('Client connected');
+
+  socket.on('close', () => {
+    console.log(`Client disconnected: ${socket.name || 'unknown'}`);
+  });
+
+  socket.on('message', msg => {
+    const data = JSON.parse(msg);
+
+    if (data.type === 'introduce') {
+      socket.name = data.name;
+      console.log(`Client introduced as: ${socket.name}`);
+      return;
+    }
+
+    socket.send(JSON.stringify({
+      reply: `Message received from ${socket.name || 'unknown'}`
+    }));
+  });
+});
